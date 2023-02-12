81e06a83e3322be905e93bcaddcc3bf98259e3da
Karol Korzeń <karol.korzen@crustlab.com>
2023-02-13 00:07:19 +0100

    Add dependency injection.

--- DummyAPI/HomeFeature/HomeDIContainer.swift
+++ DummyAPI/HomeFeature/HomeDIContainer.swift
@@ -6,3 +6,41 @@
 //
 
 import Foundation
+
+public final class HomeModuleDependencies {
+    let resolveRESTService: () -> RESTService
+    
+    public init(
+        resolveRESTService: @escaping () -> RESTService
+    ) {
+        self.resolveRESTService = resolveRESTService
+    }
+}
+
+public protocol HomeDIContainer {
+    
+}
+
+//MARK: - DefaultHomeDIContainer
+public final class DefaultHomeDIContainer: HomeDIContainer {
+    private let dependencies: HomeModuleDependencies
+    
+    public init(dependencies: HomeModuleDependencies) {
+        self.dependencies = dependencies
+    }
+}
+
+//MARK: - Interactors
+extension DefaultHomeDIContainer {
+    
+}
+
+//MARK: - Use Cases
+extension DefaultHomeDIContainer {
+    
+}
+
+//MARK: - Services
+extension DefaultHomeDIContainer {
+    
+}
